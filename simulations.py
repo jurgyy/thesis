7677ab9512e5404998e05932f0987947c9fc5b91
jurgyy <jurgen.baas@outlook.com>
2017-05-12 12:50:08 +0200

    simulations now use the same generator to loop over all patient-months.

--- simulations.py
+++ simulations.py
@@ -39,17 +39,9 @@ def get_random_subset(patients, test_rate=0.30):
     return np.array(patient_nrs[0:test_size]).tolist()
 
 
-def simulate_predictor(patients, diseases, start, end):
+def patient_month_generator(start, end, patients):
     sim_date = start
-
-    start = timeit.default_timer()
-
-    learn_data = {"Data": [], "Target": [], "Data Labels": []}
-    test_data = {"Data": [], "Target": [], "Data Labels": [], "Patients": get_random_subset(patients, test_rate=.70)}
-
-    print("Simulating...\nStart Date: {}\nEnd Date: {}".format(sim_date, end))
     while sim_date < end:
-        print(sim_date)
         for key, patient in patients.items():
             if (not patient.is_alive(sim_date)) or \
                     (not patient.has_disease_group(atrial_fib, sim_date, chronic=True)) or \
@@ -57,39 +49,37 @@ def simulate_predictor(patients, diseases, start, end):
                     patient.has_medication_group("B01", sim_date):  # Antithrombotic Agents start with B01
                 continue
 
-            if key in test_data["Patients"]:
-                add_feature_slice(test_data, diseases, patient, sim_date)
-            else:
-                add_feature_slice(learn_data, diseases, patient, sim_date)
+            yield key, patient, sim_date
 
         sim_date += relativedelta(months=+1)
 
-    stop = timeit.default_timer()
-    print("Time elapsed: {}".format(stop - start))
+
+def simulate_predictor(patients, diseases, start, end):
+    learn_data = {"Data": [], "Target": [], "Data Labels": []}
+    test_data = {"Data": [], "Target": [], "Data Labels": [], "Patients": get_random_subset(patients, test_rate=.70)}
+
+    start_timer = timeit.default_timer()
+    print("Simulating...\nStart Date: {}\nEnd Date: {}".format(start, end))
+    for key, patient, sim_date in patient_month_generator(start, end, patients):
+        if key in test_data["Patients"]:
+            add_feature_slice(test_data, diseases, patient, sim_date)
+        else:
+            add_feature_slice(learn_data, diseases, patient, sim_date)
+
+    stop_timer = timeit.default_timer()
+    print("Time elapsed: {}".format(stop_timer - start_timer))
 
     return learn_data, test_data
 
 
 def simulate_chads_vasc(patients, start, end):
-    sim_date = start
-
-    print("Simulating...\nStart Date: {}\nEnd Date: {}".format(sim_date, end))
+    print("Simulating...\nStart Date: {}\nEnd Date: {}".format(start, end))
     data = {"Data": [], "Target": []}
-    while sim_date < end:
-        print(sim_date)
-        for key, patient in patients.items():
-            if (not patient.is_alive(sim_date)) or \
-                    (not patient.has_disease_group(atrial_fib, sim_date, chronic=True)) or \
-                    patient.has_medication_group("B01", sim_date):  # Antithrombotic Agents start with B01
-                continue
-
-            data["Data"].append(1 if patient.should_have_AC(sim_date, anticoagulant_decision.chads_vasc,
-                                                            {"max_value": 3}) else 0)
-            data["Target"].append(1 if patient.should_have_AC(sim_date, anticoagulant_decision.future_stroke,
-                                                              {"months": 12}) else 0)
-
-        sim_date += relativedelta(months=+1)
-
+    for key, patient, sim_date in patient_month_generator(start, end, patients):
+        data["Data"].append(1 if patient.should_have_AC(sim_date, anticoagulant_decision.chads_vasc,
+                                                        {"max_value": 3}) else 0)
+        data["Target"].append(1 if patient.should_have_AC(sim_date, anticoagulant_decision.future_stroke,
+                                                          {"months": 12}) else 0)
     return data
 
 
@@ -100,7 +90,7 @@ def compare_predictor_chads_vasc(patients, diseases):
 
     start_date = datetime.date(2005, 1, 1)
     # end_date = datetime.date(2015, 6, 1)
-    end_date = datetime.date(2009, 7, 1)
+    end_date = datetime.date(2007, 7, 1)
 
     chads_vasc = simulate_chads_vasc(patients, start_date, end_date)
     # TODO: Find the true adjusted stroke rate
@@ -110,7 +100,7 @@ def compare_predictor_chads_vasc(patients, diseases):
     cf_chads_vasc = analyze_chads_vasc(chads_vasc)
 
     print("Predicting...")
-    cf_prediction = predict(learn, test, plot=False)
+    cf_prediction = predict(learn, test, plot=True)
 
     cf_chads_vasc.dump()
     cf_prediction.dump()
