ed9985ac4142bf8110df73cea3b061d1629b5a68
jurgyy <jurgen.baas@outlook.com>
2017-05-12 16:32:42 +0200

    Improved indentations

--- tests/test_csv_readers.py
+++ tests/test_csv_readers.py
@@ -19,10 +19,9 @@ class TestCsvReaders(TestCase):
     def test_get_patients(self):
         self.assertEqual(self.patients[5], Patient(5, "m", d(1931, 2, 3), d(datetime.MAXYEAR, 12, 31)))
         self.assertEqual(self.patients[123], Patient(123, "v", d(1999, 10, 11), d(2015, 1, 1)))
-        self.assertEqual(self.patients, {
-            5: Patient(5, "m", d(1931, 2, 3), d(datetime.MAXYEAR, 12, 31)),
-            123: Patient(123, "v", d(1999, 10, 11), d(2015, 1, 1))
-        })
+
+        self.assertEqual(self.patients, {5: Patient(5, "m", d(1931, 2, 3), d(datetime.MAXYEAR, 12, 31)),
+                                         123: Patient(123, "v", d(1999, 10, 11), d(2015, 1, 1))})
 
     def test_get_diagnoses(self):
         disease_TE1_000 = Disease("TE1", "000", description="Test Disease 1")
@@ -30,16 +29,18 @@ class TestCsvReaders(TestCase):
         disease_TE2_001 = Disease("TE2", "001", description="Test Disease 3")
         disease_TE3_001 = Disease("TE3", "001", description="Test Disease 4")
 
-        self.assertEqual(self.diagnoses, {
-            5: [Diagnosis(disease_TE1_000, d(2010, 1, 1), d(2010, 10, 31), "John Deer"),
-                Diagnosis(disease_TE1_000, d(2010, 11, 1), d(2010, 12, 31), "Jane Doe"),
-                Diagnosis(disease_TE1_001, d(2010, 5, 1), d(2010, 6, 1), "John Deer"),
-                Diagnosis(disease_TE2_001, d(2010, 6, 2), d(2011, 10, 1), "John Deer"),
-                Diagnosis(disease_TE3_001, d(2010, 3, 18), d(2010, 4, 19), "John Deer")],
-            123: [Diagnosis(disease_TE1_000, d(2010, 1, 1), d(2010, 1, 31), "John Deer"),
-                  Diagnosis(disease_TE2_001, d(2011, 6, 3), d(2012, 6, 2), "John Deer"),
-                  Diagnosis(disease_TE2_001, d(2015, 1, 1), d(2015, 5, 1), "John Deer")]
-        })
+        diagnoses_p5 = [Diagnosis(disease_TE1_000, d(2010, 1, 1), d(2010, 10, 31), "John Deer"),
+                        Diagnosis(disease_TE1_000, d(2010, 11, 1), d(2010, 12, 31), "Jane Doe"),
+                        Diagnosis(disease_TE1_001, d(2010, 5, 1), d(2010, 6, 1), "John Deer"),
+                        Diagnosis(disease_TE2_001, d(2010, 6, 2), d(2011, 10, 1), "John Deer"),
+                        Diagnosis(disease_TE3_001, d(2010, 3, 18), d(2010, 4, 19), "John Deer")]
+
+        diagnoses_p123 = [Diagnosis(disease_TE1_000, d(2010, 1, 1), d(2010, 1, 31), "John Deer"),
+                          Diagnosis(disease_TE2_001, d(2011, 6, 3), d(2012, 6, 2), "John Deer"),
+                          Diagnosis(disease_TE2_001, d(2015, 1, 1), d(2015, 5, 1), "John Deer")]
+
+        self.assertEqual(self.diagnoses,
+                         {5: diagnoses_p5, 123: diagnoses_p123})
 
     def test_get_medications(self):
         self.assertEqual(self.medications, {5: [Medication("A00AA00", d(2010, 1, 1), d(datetime.MAXYEAR, 12, 31)),
