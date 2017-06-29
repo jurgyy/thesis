c700add3e36ad4c2d422b4a30ee4629686d46ce2
jurgyy <jurgen.baas@outlook.com>
2017-06-29 17:07:50 +0200

    Changed main to match all previous commits.

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import datetime
 import matplotlib.pyplot as plt
 
 import disease_groups
+from breakdown import breakdown
 from csv_reader.diagnose_csv import get_diagnoses
 from csv_reader.medication_csv import get_medications
 from csv_reader.patients_csv import get_patients
+from csv_reader.mergers import *
 from practioner_analysis.practitioner import analyze_practitioners
 
 
@@ -18,7 +20,11 @@ def add_diseases(patients, diagnoses):
 def add_medications(patients, medications):
     for patient_nr, medication in medications.items():
         for m in medication:
-            patients[patient_nr].add_medication(m)
+            try:
+                patients[patient_nr].add_medication(m)
+            except KeyError:
+                print("Missing data, patient {}".format(patient_nr))
+                continue
 
 
 def get_all_diseases(diagnoses):
@@ -63,26 +69,22 @@ def plot_disease_frequency(diseases, diagnoses):
     plt.show()
 
 
-def prepare_data():
+def prepare_data(dir, sep='\t'):
     print("Reading CSV files...")
-    patients = get_patients("data/msc_A/patient_general.csv")
-    diagnoses = get_diagnoses("data/msc_A/patient_diagnoses.csv", merge=False)
-    medications = get_medications("data/msc_A/patient_meds.csv")
+    patients = get_patients("data/{}/patient_general.csv".format(dir), sep)
+    diagnoses = get_diagnoses("data/{}/patient_diagnoses.csv".format(dir), sep, merge=False)
+    medications = get_medications("data/{}/patient_meds.csv".format(dir), sep)
 
+    print("Linking data...")
     add_diseases(patients, diagnoses)
     add_medications(patients, medications)
 
-    diseases = get_all_diseases(diagnoses)
-
-    # Optionally remove rare disease which reduces the feature space roughly in half
-    # diseases = reduce_feature_space(diseases, diagnoses, min_frequency=10)
-
-    # plot_disease_frequency(diseases, diagnoses)
+    print("Finding events...")
     for p in patients.values():
         p.find_strokes()
         p.find_chads_vasc_changes()
 
-    return patients, diagnoses, diseases
+    return patients, diagnoses, medications
 
 
 def get_chads_vasc_diseases():
@@ -94,14 +96,34 @@ def get_chads_vasc_diseases():
 
 
 def main():
-    patients, diagnoses, diseases = prepare_data()
+    patients_A, diagnoses_A, medications_A = prepare_data("msc_A", sep='\t')
+    patients_B, diagnoses_B, medications_B = prepare_data("msc_B", sep='\t')
+
+    print("Merging data...")
+    patients = merge_data(patients_A, patients_B)
+    diagnoses = merge_data(diagnoses_A, diagnoses_B)
+    medications = merge_data(medications_A, medications_B)
+
+    print("Total number of patients: {}".format(len(patients.keys())))
+    print("Number of female patients: {}".format(sum([1 if p.is_female() else 0 for p in patients.values()])))
+
+    diseases = get_all_diseases(diagnoses)
+
+    # Optionally remove rare disease which reduces the feature space roughly in half
+    # diseases = reduce_feature_space(diseases, diagnoses, min_frequency=10)
+    # plot_disease_frequency(diseases, diagnoses)
+
     start = datetime.date(2005, 1, 1)
     start = datetime.date(2013, 4, 1)
     end = datetime.date(2017, 6, 1)
     # end = datetime.date(2007, 7, 1)
 
+    breakdown(patients, datetime.date(2017, 1, 1))
     # compare_predictor_chads_vasc(patients, diseases, start, end, load_from_file=False)
-    analyze_practitioners(patients, start, end)
+    analyze_practitioners(patients_A, start, end, spec="CAR", diag="401", meds_start_with="B01",
+                          plot=True, fname_prefix="A_")
+    analyze_practitioners(patients_B, start, end, spec="CAR", diag="401", meds_start_with="B01",
+                          plot=True, fname_prefix="B_")
     # asr = find_adjusted_stroke_rate(patients, start, end)
     # print(asr)
 
