b96cfd50947ae33e822f58a11940599008ed55e9
jurgyy <jurgen.baas@outlook.com>
2017-07-04 17:03:10 +0200

    Added feature selection and F1 score. Added contour plot (WIP).

--- learning/plot.py
+++ learning/plot.py
@@ -1,4 +1,5 @@
 import pydot
+import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.tree import export_graphviz
 
@@ -25,7 +26,7 @@ def plot_features(clf, labels):
 
 
 def plot_trees(clf, labels, n=3):
-    print("Outputting first three trees")
+    print("Outputting first {} trees".format(n))
     for i in range(n):
         export_graphviz(clf.estimators_[i],
                         feature_names=labels,
@@ -34,3 +35,36 @@ def plot_trees(clf, labels, n=3):
                         out_file="output/tree.dot")
         (graph,) = pydot.graph_from_dot_file('output/tree.dot')
         graph.write_png('output/new_tree_{}.png'.format(i), prog="graphviz/bin/dot.exe")
+
+
+def plot_surface(clf, x, y, labels=None, cutoff=None):
+    plt.figure()
+    print("Plotting surface...")
+    importances, indexes = zip(*sorted(zip(clf.feature_importances_, range(len(x[0]))), reverse=True))
+    x = np.array(x)[:, indexes[:2]]
+
+    x_min, x_max = x[:, 0].min(), x[:, 0].max()
+    y_min, y_max = x[:, 1].min(), x[:, 1].max()
+    x_step = (x_max - x_min) / 100
+    y_step = (y_max - y_min) / 100
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, x_step), np.arange(y_min, y_max, y_step))
+
+    clf.fit(x, y)
+    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+    Z = Z.reshape(xx.shape)
+
+    cs = plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm)
+    cbar = plt.colorbar(cs)
+    cbar.ax.set_ylabel('Probability')
+    if cutoff is not None:
+        cs_lines = plt.contour(cs, levels=[cutoff], colors='black')
+        cbar.add_lines(cs_lines)
+
+    plt.scatter(x[:, 0], x[:, 1], c=y, cmap=plt.cm.Paired, alpha=0.2)
+
+    if labels is not None:
+        plt.xlabel(labels[indexes[0]])
+        plt.ylabel(labels[indexes[1]])
+
+    plt.title("Surface plot of two most important features")
+    plt.savefig("output/surface.png", bbox_inches='tight')
--- learning/predictor.py
+++ learning/predictor.py
@@ -1,7 +1,10 @@
+import numpy as np
 from sklearn import ensemble
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score
 
 from learning.confusion_matrix import ConfusionMatrix
-from learning.plot import plot_features, plot_trees
+from learning.plot import plot_features, plot_trees, plot_surface
 
 
 def analyze_chads_vasc(data):
@@ -9,25 +12,66 @@ def analyze_chads_vasc(data):
     return cf
 
 
-def predict(learn_features, learn_target, test_features, test_target, labels, plot=False):
+def custom_f1(cutoff):
+    def f1_cutoff(clf, x, y):
+        ypred = np.array(clf.predict_proba(x)[:, 1] > cutoff).astype(int)
+        return f1_score(y, ypred)
+
+    return f1_cutoff
+
+
+def predict(learn_x, learn_y, test_x, test_y, labels, n_features=.2, cutoff=.1, plot=False):
     clf = ensemble.RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight='balanced')
 
-    print("# Learn Data Size: {}".format(len(learn_features)))
-    print("# Positive Target: {}".format(sum(learn_target)))
-    print("# Test Data Size:  {}".format(len(test_features)))
-    print("# Positive Target: {}".format(sum(test_target)))
-    print("# Features:        {}".format(len(learn_features[0])))
+    print("# Learn Data Size: {}".format(len(learn_x)))
+    print("# Positive Target: {}".format(sum(learn_y)))
+    print("# Test Data Size:  {}".format(len(test_x)))
+    print("# Positive Target: {}".format(sum(test_y)))
+    print("# Features:        {}".format(len(learn_x[0])))
 
-    print("Fitting... ")
-    clf.fit(learn_features, learn_target)
+    print("Fitting Whole Dataset... ")
+    clf.fit(learn_x, learn_y)
+
+    print("Reducing Feature Space... ")
+    importances, indexes = zip(*sorted(zip(clf.feature_importances_, range(len(learn_x[0]))), reverse=True))
+
+    if type(n_features) == float:
+        n_features = int(len(importances) * n_features)
+
+    exclude_indeces = indexes[n_features:]
+    learn_x = np.delete(learn_x, exclude_indeces, axis=1)
+    test_x = np.delete(test_x, exclude_indeces, axis=1)
+    labels = np.delete(labels, exclude_indeces)
+
+    if cutoff is None:
+        print("Finding Correct Probability Cutoff...")
+
+        # In the unbalanced data set the cutoff will most likely be below .5,
+        # to safe time, no cutoffs > .5 are checked.
+        cutoffs = np.arange(0.05, 0.5, 0.05)
+        highest_mean, highest_cutoff = 0, 0
+        for c in cutoffs:
+            validated = cross_val_score(clf, learn_x, learn_y, cv=10, scoring=custom_f1(c))
+            mean = np.mean(validated)
+
+            print("Cutoff: {} with mean: {}".format(c, mean))
+            if mean > highest_mean:
+                highest_mean = mean
+                highest_cutoff = c
+
+        if highest_cutoff == 0:
+            raise NotImplementedError
+
+        print("Best Cutoff: {} with mean: {}".format(highest_cutoff, highest_mean))
+        cutoff = highest_cutoff
+
+    print("Fitting Reduced Dataset...")
+    clf.fit(learn_x, learn_y)
+
     print("Predicting...")
-    predictions = clf.predict(test_features)
-    print("Predictions:\n{}, {}".format(sum(predictions), predictions))
-    # for i in range(len(predictions)):
-    #     if predictions[i]:
-    #         print(predictions[i], test_target[i])
+    predictions = [1 if p[1] > cutoff else 0 for p in clf.predict_proba(test_x)]
 
-    cf = ConfusionMatrix(test_target, predictions, name="Random Forest")
+    cf = ConfusionMatrix(test_y, predictions, name="Random Forest")
 
     if plot:
         try:
@@ -35,8 +79,10 @@ def predict(learn_features, learn_target, test_features, test_target, labels, pl
         except:
             print("Error plotting features. Continuing...")
         try:
+            # plot_trees(clf, labels, n=1)
             pass
-            # plot_trees(clf, labels, n=3)
         except:
             print("Error plotting trees. Continuing...")
+
+        plot_surface(clf, learn_x, learn_y, labels=labels)
     return cf
