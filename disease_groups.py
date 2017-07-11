41a89db250f60c49d72a172bc0fc19198bcd1cbf
jurgyy <jurgen.baas@outlook.com>
2017-07-11 17:06:22 +0200

    Description added to the groups.

--- disease_groups.py
+++ disease_groups.py
@@ -13,55 +13,55 @@ from disease import Disease
 # Anesasie      ANE
 
 # TODO: Check the final diagnoses list for all possible diseases
-chads_vasc_c = [Disease("INT", "107"),  # Decompensatio cordis
-                Disease("CAR", "301"),  # Acuut hartfalen
-                Disease("CAR", "302"),  # chronisch hartfalen
-                Disease("CAR", "802"),  # followup na PTCA e/o CABG/abl
-                Disease("GER", "262"),  # Decompensatio cordis
-                Disease("CAR", "203"),  # angina pectoris, onstabiel
-                Disease("PLA", "352")]  # decompr overige compr syndr
+chads_vasc_c = [Disease("INT", "107", description="Decompensatio cordis"),
+                Disease("CAR", "301", description="Acuut hartfalen"),
+                Disease("CAR", "302", description="chronisch hartfalen"),
+                Disease("CAR", "802", description="followup na PTCA e/o CABG/abl"),
+                Disease("GER", "262", description="Decompensatio cordis"),
+                Disease("CAR", "203", description="angina pectoris, onstabiel"),
+                Disease("PLA", "352", description="decompr overige compr syndr")]
 chads_vasc_h = [Disease("7", "901"),
-                Disease("CAR", "902"),  # hypertensie
-                Disease("INT", "311")]  # Hypertensie
+                Disease("CAR", "902", description="hypertensie"),
+                Disease("INT", "311", description="Hypertensie")]
 chads_vasc_d = [Disease("7", "902"),
-                Disease("INT", "221"),  # DM znd secundaire complicaties
-                Disease("INT", "222"),  # DM met secundaire complicaties
-                Disease("INT", "223"),  # DM chronisch pomptherapie
-                Disease("CHI", "432")]  # diabetische voet(diabetes nno)
+                Disease("INT", "221", description="DM znd secundaire complicaties"),
+                Disease("INT", "222", description="DM met secundaire complicaties"),
+                Disease("INT", "223", description="DM chronisch pomptherapie"),
+                Disease("CHI", "432", description="diabetische voet(diabetes nno)")]
 chads_vasc_s = [Disease("INT", "101"),
-                Disease("INT", "121"),  # cerebrovasculair acc/TIA
-                Disease("OGK", "101"),  # CVA
-                Disease("NEU", "1111"), # onbloedige beroerte
-                Disease("NEU", "1112"), # TIA (incl amaurosis fugax)
-                Disease("NEU", "1199"), # Overige cerebrovasc aand
-                Disease("NEU", "9927"), # Geen neur, werkdiag TIA
-                Disease("GER", "263"),  # CVA / TIA
-                Disease("REV", "0313")] # CVA
-chads_vasc_v = [Disease("INT", "102"),  # Instabiele AP, myocardinfarct
+                Disease("INT", "121", description="cerebrovasculair acc/TIA"),
+                Disease("OGK", "101", description="CVA"),
+                Disease("NEU", "1111", description="onbloedige beroerte"),
+                Disease("NEU", "1112", description="TIA (incl amaurosis fugax)"),
+                Disease("NEU", "1199", description="overige cerebrovasc aand"),
+                Disease("NEU", "9927", description="Geen neur, werkdiag TIA"),
+                Disease("GER", "263", description="CVA / TIA"),
+                Disease("REV", "0313", description="CVA")]
+chads_vasc_v = [Disease("INT", "102", description="Instabiele AP, myocardinfarct"),
                 Disease("INT", "104"),
-                Disease("INT", "124"),  # atheroscl extr/perif vaatlijd
-                Disease("GER", "261"),  # Aandoeningen hartvaatstelsel
-                Disease("OOG", "657"),  # vaatafsluiting
-                Disease("ANE", "110"),  # Ischaem pijn a/d extremiteiten
-                Disease("CHI", "406"),  # aneurysma aorta abd, ruptuur
-                Disease("CHI", "402"),  # Carotispathologie
-                Disease("CHI", "405"),  # Aneurysma aorta iliacaal
-                Disease("CHI", "409"),  # Vaat afw abdominaal / bekken
-                Disease("CHI", "412"),  # P.A.O.D. arm
-                Disease("CHI", "417"),  # Arteriële embolie+trombose,336
-                Disease("CHI", "418"),  # P.A.O.D. 2, claudicatio interm
-                Disease("CHI", "419"),  # P.A.O.D. 3, rustpijn
-                Disease("CHI", "420"),  # P.A.O.D. 4, gangreen
-                Disease("RAD", "183"),  # Arteria iliaca communis
-                Disease("RAD", "187"),  # Aorta-iliacaal
-                Disease("RAD", "191"),  # Arteria femoralis communis
-                Disease("RAD", "192"),  # Arteria femoral superficialis
-                Disease("RAD", "194"),  # Arteria poplitea
-                Disease("CAR", "204"),  # ST elevatie hartinfarct
-                Disease("CAR", "205"),  # Non ST elevatie hartinfarct
-                Disease("CAR", "601"),  # Arteriele vaatafw / stenose
-                Disease("CAR", "801"),  # Follow-up na acuut cor syndr
-                Disease("CAR", "808")]  # f-up na vaatoper(arte/ven)
+                Disease("INT", "124", description="atheroscl extr/perif vaatlijd"),
+                Disease("GER", "261", description="Aandoeningen hartvaatstelsel"),
+                Disease("OOG", "657", description="vaatafsluiting"),
+                Disease("ANE", "110", description="Ischaem pijn a/d extremiteiten"),
+                Disease("CHI", "406", description="aneurysma aorta abd, ruptuur"),
+                Disease("CHI", "402", description="Carotispathologie"),
+                Disease("CHI", "405", description="Aneurysma aorta iliacaal"),
+                Disease("CHI", "409", description="Vaat afw abdominaal / bekken"),
+                Disease("CHI", "412", description="P.A.O.D. arm"),
+                Disease("CHI", "417", description="Arteriële embolie+trombose,336"),
+                Disease("CHI", "418", description="P.A.O.D. 2, claudicatio interm"),
+                Disease("CHI", "419", description="P.A.O.D. 3, rustpijn"),
+                Disease("CHI", "420", description="P.A.O.D. 4, gangreen"),
+                Disease("RAD", "183", description="Arteria iliaca communis"),
+                Disease("RAD", "187", description="Aorta-iliacaal"),
+                Disease("RAD", "191", description="Arteria femoralis communis"),
+                Disease("RAD", "192", description="Arteria femoral superficialis"),
+                Disease("RAD", "194", description="Arteria poplitea"),
+                Disease("CAR", "204", description="ST elevatie hartinfarct"),
+                Disease("CAR", "205", description="Non ST elevatie hartinfarct"),
+                Disease("CAR", "601", description="Arteriele vaatafw / stenose"),
+                Disease("CAR", "801", description="Follow-up na acuut cor syndr"),
+                Disease("CAR", "808", description="f-up na vaatoper(arte/ven)")]
 
 # Currently a copy of chads_vasc_s but might differ in the future
 stroke_diseases = chads_vasc_s
