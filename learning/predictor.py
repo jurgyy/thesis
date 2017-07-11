40fdd15488e2952e03d0cce6f8ff8d60e1df84e1
jurgyy <jurgen.baas@outlook.com>
2017-07-11 17:04:41 +0200

    cross validation now uses GroupKFold so the same patients are not in both the learn and cross validation sets. Added cutoff box plot call.

--- learning/predictor.py
+++ learning/predictor.py
@@ -1,29 +1,30 @@
 import numpy as np
 from sklearn import ensemble
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GroupKFold
 from sklearn import metrics
 
 from learning.confusion_matrix import ConfusionMatrix
-from learning.plot import plot_features, plot_trees, plot_surface
+from learning.plot import *
 
 
 def analyze_chads_vasc(data):
-    cf = ConfusionMatrix(data["Target"], data["Data"], name="CHADS-VASc")
-    return cf
+    cm = ConfusionMatrix(data["Target"], data["Data"], name="CHADS-VASc")
+    return cm
 
 
-def custom_f1(cutoff):
-    def f1_cutoff(clf, x, y):
+def custom_score(cutoff, beta):
+    def score_cutoff(clf, x, y):
         ypred = np.array(clf.predict_proba(x)[:, 1] > cutoff).astype(int)
+        cm = ConfusionMatrix(y, ypred)
+        r = cm.tpr
+        s = cm.tnr
+        print(cm.calculate_matrix(), r, s)
+        return (beta ** 2 + 1) * (s * r) / (beta ** 2 * s + r)
 
-        # cf = ConfusionMatrix(ypred, y)
-
-        return metrics.recall_score(y, ypred)
-
-    return f1_cutoff
+    return score_cutoff
 
 
-def predict(learn_x, learn_y, test_x, test_y, labels, n_features=.2, cutoff=.1, plot=False):
+def predict(learn_x, learn_y, groups, test_x, test_y, labels, n_features=.2, cutoff=.1, plot=False):
     clf = ensemble.RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight='balanced')
 
     print("# Learn Data Size: {}".format(len(learn_x)))
@@ -52,14 +53,20 @@ def predict(learn_x, learn_y, test_x, test_y, labels, n_features=.2, cutoff=.1, 
         # In the unbalanced data set the cutoff will most likely be below .5,
         # to safe time, no cutoffs > .5 are checked.
 
-        print(np.mean(cross_val_score(clf, learn_x, learn_y, cv=10, scoring='roc_auc')))
-        cutoffs = np.arange(0.05, 0.5, 0.05)
+        gkf = GroupKFold(n_splits=10)
+
+        cutoffs = np.arange(0.025, 0.275, 0.025)
+        beta = 2
+        cutoff_scores = []
         highest_mean, highest_cutoff = 0, 0
         for c in cutoffs:
-            validated = cross_val_score(clf, learn_x, learn_y, cv=10, scoring=custom_f1(c))
+            tmp_clf = ensemble.RandomForestClassifier(n_estimators=100, n_jobs=-1, class_weight='balanced')
+            validated = cross_val_score(tmp_clf, learn_x, learn_y, cv=gkf.split(learn_x, learn_y, groups),
+                                        scoring=custom_score(c, beta))
+            cutoff_scores.append(validated)
             mean = np.mean(validated)
 
-            print("Cutoff: {} with mean: {}".format(c, mean))
+            print("Cutoff: {} with mean: {} | {}".format(c, mean, validated))
             if mean > highest_mean:
                 highest_mean = mean
                 highest_cutoff = c
@@ -70,13 +77,16 @@ def predict(learn_x, learn_y, test_x, test_y, labels, n_features=.2, cutoff=.1, 
         print("Best Cutoff: {} with mean: {}".format(highest_cutoff, highest_mean))
         cutoff = highest_cutoff
 
+        if plot:
+            plot_cutoffs(cutoffs, cutoff_scores, ylabel="$S_{%s}$ Score" % beta)
+
     print("Fitting Reduced Dataset...")
     clf.fit(learn_x, learn_y)
 
     print("Predicting...")
     predictions = [1 if p[1] > cutoff else 0 for p in clf.predict_proba(test_x)]
 
-    cf = ConfusionMatrix(test_y, predictions, name="Random Forest")
+    cm = ConfusionMatrix(test_y, predictions, name="Random Forest")
 
     if plot:
         try:
@@ -90,4 +100,4 @@ def predict(learn_x, learn_y, test_x, test_y, labels, n_features=.2, cutoff=.1, 
             print("Error plotting trees. Continuing...")
 
         plot_surface(clf, learn_x, learn_y, labels=labels, cutoff=cutoff)
-    return cf
+    return cm
